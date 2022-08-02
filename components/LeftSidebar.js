cf078550c739e692ea1d9adbd920ea3d683b9fe4
chakib2002 <abdelmalek.feddal@worldlearningalgeria.org>
2022-08-02 17:54:49 +0100

    leftSidebar + rightSidebar components done

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -1,17 +1,22 @@
-import { XIcon } from '@heroicons/react/outline'
+import { ArrowCircleLeftIcon, XIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 export default function LeftSidebar({isOpen, setLeftSidebar}) {
   return (  
-    <div className={`${!isOpen ? "-left-96" : "left-0"} h-screen fixed ease-in-out duration-700 bg-white shadow-md shadow-slate-400`} >
-      <div className=''>
-        <XIcon className='h-6 w-6 text-right right-3 top-3 absolute ' onClick={()=> setLeftSidebar(false)}/>
+    <div className={`${!isOpen ? "-left-96" : "left-0"} rounded-r-2xl h-screen fixed ease-in-out duration-700 bg-white shadow-md shadow-slate-400`} >
+      <div className='cursor-pointer px-2 py-2 mx-2 my-2 space-x-3 mb-12'>
+        <ArrowCircleLeftIcon className='h-6 w-6 right-5 absolute ' onClick={()=> setLeftSidebar(false)}/>
       </div>
-      <ul className='my-10 ml-5 mr-14'>
-        <li className='py-2 my-4 text-sm font-light'>ALL</li>
-        <li className='py-2 my-4 text-sm font-light'>NEW ARRIVALS</li>
-        <li className='py-2 my-4 text-sm font-light'>COLLECTIONS</li>
-        <li className='py-2 my-4 text-sm font-light'>TRENDS</li>
+      <h1 className='mt-5 ml-4 text-sm'>OPTIONS :</h1>
+      <ul className='mx-3 ml-4'>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+          active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>ALL</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+          active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>NEW ARRIVALS</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+          active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>COLLECTIONS</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+          active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>TRENDS</li>
       </ul>
     </div>
   )
--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -1,18 +1,26 @@
-import { XIcon } from '@heroicons/react/outline'
+import { ArrowCircleRightIcon, XIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 export default function RightSidebar({isOpen, setRightSidebar}) {
   return (
-    <div className={`${!isOpen ? "-right-96" : "right-0"}  h-screen fixed ease-in-out duration-700 bg-white shadow-md shadow-slate-400`} >
-      <div className=''>
-        <XIcon className='h-6 w-6 text-right left-3 top-3 absolute' onClick={()=> setRightSidebar(false)}/>
+    <div className={`${!isOpen ? "-right-96" : "right-0"} rounded-l-2xl h-screen fixed ease-in-out duration-700 bg-white shadow-md shadow-slate-400`} >
+      <div className=' inline-flex cursor-pointer px-2 py-2 mx-2 my-2 space-x-3' 
+        onClick={()=> setRightSidebar(false)}>
+        <ArrowCircleRightIcon className='h-6 w-6 self-center text-gray-900'/>
+        
       </div>
-      <ul className='my-10 mx-12 text-center'>
-        <li className='py-2 my-4 text-sm font-light'>CLOTHING</li>
-        <li className='py-2 my-4 text-sm font-light'>SHOES</li>
-        <li className='py-2 my-4 text-sm font-light'>HATS</li>
-        <li className='py-2 my-4 text-sm font-light'>COATS</li>
-        <li className='py-2 my-4 text-sm font-light'>ACCESSORIES</li>
+      <h1 className='mt-5 ml-4 text-sm'>CATEGORIES :</h1>
+      <ul className='mx-3 ml-4 '>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+        active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>CLOTHING</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+        active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>SHOES</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+        active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>HATS</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+        active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>COATS</li>
+        <li className='py-2 my-4 border-l border-gray-700 px-1 w-full bg-gray-200 pr-20 text-xs 
+        active:scale-95 transition duration-300 ease-out cursor-pointer font-thin active:bg-gray-600 active:text-white'>ACCESSORIES</li>
       </ul>
     </div>
   )
